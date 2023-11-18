69957ffd51d7be7d4db18145c5011e2f8c389ded
anto1313 <anthorouw@gmail.com>
2023-11-18 09:41:55 +0700

    #1 Commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import Typed from 'typed.js';
+import confetti from 'canvas-confetti';
+import SiriWave from "siriwave";
+
+
+
+const typed = new Typed('#element', {
+  strings: ["'Bayangkan jika kita tidak <i>menyerah</i>'", "<i>'tantangan</i> apapun dari ayah'", "'atau dunia kita <i>lewati'</i>","'kita <i>hadapi'</i>"],
+  typeSpeed: 50,
+});
+
+const appBtn = document.getElementById("app");
+appBtn.addEventListener("click", () => {
+
+const myCanvas = document.createElement("canvas");
+myCanvas.style.position = "fixed";
+myCanvas.style.inset = 0;
+myCanvas.style.width = "100vw";
+myCanvas.style.height = "100vh";
+myCanvas.style.zIndex = -1;
+document.body.appendChild(myCanvas);
+
+const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true
+  });
+  myConfetti({
+    particleCount: 100,
+    spread: 160
+  });
+})
+
